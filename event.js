fd6906111b7289265559358905b91a00c6023eed
kaohon <gudoumaki@gmail.com>
2024-02-12 23:28:05 +0900

    collection css fixed #1

--- event.js
+++ event.js
@@ -85,6 +85,24 @@ stats_open.addEventListener("click", function () {
     items.style.display = "none";
     pass.style.display = "none";
   });
+
+  ct_01_open.addEventListener("click", function () {
+    ct_01.style.display = "block";
+    ct_02.style.display = "none";
+    ct_demo.style.display = "none";
+  });
+
+  ct_02_open.addEventListener("click", function () {
+    ct_02.style.display = "block";
+    ct_01.style.display = "none";
+    ct_demo.style.display = "none";
+  });
+
+  ct_demo_open.addEventListener("click", function () {
+    ct_demo.style.display = "block";
+    ct_02.style.display = "none";
+    ct_01.style.display = "none";
+  });
   
   eatcandy_button.addEventListener("click", function () {
     candy -= candy;
