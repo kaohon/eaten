596557756f26a538c1945e8f08572f87ad4cc38c
kaohon <gudoumaki@gmail.com>
2024-03-27 10:23:47 +0900

    Add level system and level achivement

--- level.js
+++ level.js
@@ -1,12 +1,60 @@
-const levelUps = new Array(100).fill(false);
+counthour = parseInt(elapsedhour.innerHTML);
+levelhtml = document.getElementById("levelhtml");
 
-const lvup = () => {
-  for (let i = 0; i < levelUps.length; i++) {
-    const level = i + 1;
-    const element = document.querySelector(`#l${level}`);
-    if (counthour >= level * 10 && !levelUps[i]) {
-      element.classList.add("lup");
-      levelUps[i] = true;
+const levelup = () => {
+    const levels = [];
+    for (let i = 1; i <= 500; i++) {
+        levels.push({ hour: i * 10, now: `${i}` });
     }
-  }
-};
+
+    levels.forEach(level => {
+        if (counthour >= level.hour) {
+            levelhtml.innerHTML = level.now;
+        }
+    });
+
+    if (counthour >= 500) {
+      l50.style.display = "block";
+    }
+
+    if (counthour >= 1000) {
+      l100.style.display = "block";
+    }
+
+    if (counthour >= 1500) {
+      l150.style.display = "block";
+    }
+
+    if (counthour >= 2000) {
+      l200.style.display = "block";
+    }
+
+    if (counthour >= 2500) {
+      l250.style.display = "block";
+    }
+
+    if (counthour >= 3000) {
+      l300.style.display = "block";
+    }
+
+    if (counthour >= 3500) {
+      l350.style.display = "block";
+    }
+
+    if (counthour >= 4000) {
+      l400.style.display = "block";
+    }
+
+    if (counthour >= 4500) {
+      l450.style.display = "block";
+    }
+
+    if (counthour >= 5000) {
+      l500.style.display = "block";
+    }
+}
+
+setInterval(() => {
+    counthour = parseInt(elapsedhour.innerHTML);
+    levelup();
+}, 1000);
